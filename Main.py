935924c467c3803454016093c51746a2a0ac3b1c
HariniSundaram <77590326+HariniSundaram@users.noreply.github.com>
2022-02-05 11:50:54 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,25 +1,22 @@
 
 class Task(object):
-    def __init__(self, event, summary, duration, frequency, by_day, until, preferance_list, isFlexible, location, start_time = None, end_time = None): 
+    def __init__(self, event, description, duration, weekly_recurrance, preferance_list, isFlexible, location, time_zone, start_time = None, end_time = None): 
         # string
         self.name = event 
-        self.summary = summary
-        
-        # time in minutes? Will figure out
+        self.description = description
+        # hour:minutes:secon
         self.duration = duration
         self.start_time = start_time
         self.end_time = end_time
-        
-        self.frequency = frequency
-        self.by_day = weekly_recurrance
-        self.until = until
-        
+        # Mo,TU, WE, TH, FR
+        self.byDay = weekly_recurrance
         # list of day/time prefances...will figure out format...
         self.preferance_list = preferance_list
         # binary true false or 0/1
         self.isFlexible = isFlexible
         # string -> look at google to see what we need / what format we envision
         self.location = location
+        self.time_zone = time_zone
 
 
 class Veronica(object): 
@@ -27,10 +24,16 @@ class Veronica(object):
         self.max_block_length = max_block_length
         self.max_adj_block = max_adj_block
         self.task_list = []
+        self.flexible_task_list = []
+        self.inflexible_task_list = []
         # will need binary variable for seeing whether they want to update 
         #will add as needed. 
 
-    def add_task(self, event, summary, duration, frequency, by_day, until, preferance_list, isFlexible, location, start_time, end_time): 
-        new_task = Task(event, duration, weekly_recurrance, preferance_list, isFlexible, location)
+    def add_task(self, event, description, duration, weekly_recurrance, preferance_list, isFlexible, location, time_zone, start_time = None, end_time = None): 
+        new_task = Task(event, description, duration, weekly_recurrance, preferance_list, isFlexible, location, time_zone, start_time, end_time)
         self.task_list.append(new_task)
+        if isFlexible: 
+            self.flexible_task_list.append(new_task)
+        else: 
+            self.inflexible_task_list.append(new_task)
 
